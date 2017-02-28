461720e87fc78ec9f0358619a7ed088e96ad25fe
Maryann13 <ann-bolotina@yandex.ru>
2017-02-28 09:18:22 +0300

    Add and implement class Square

---
+++ SimpleGeometry/GeometryLib/Shapes/Square.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace GeometryLib.Shapes
+{
+    public class Square : Shape
+    {
+        public double Side { get; }
+
+        public Square(double side)
+        {
+            Debug.Assert(side > 0, "side > 0");
+            Side = side;
+        }
+
+        public override string Print()
+        {
+            return $"Square: side = {Side}, area = {Area()}";
+        }
+
+        public override double Area()
+        {
+            return Side * Side;
+        }
+    }
+}
